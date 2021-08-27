08e8bba1b756a9aba1bed798d1372691eec981bb
Gerant <geraskin.antony@yandex.ru>
2021-08-27 10:06:26 +0700

    продукт в таблице редактируется, но изменени не сохраняются в json

--- src/main/kotlin/app/MyFragment.kt
+++ src/main/kotlin/app/MyFragment.kt
@@ -3,18 +3,23 @@ package app
 import javafx.collections.ObservableList
 import tables.Products
 import tornadofx.Fragment
-import tornadofx.readonlyColumn
+import tornadofx.column
+import tornadofx.makeEditable
 import tornadofx.tableview
 
 class MyFragment: Fragment() {
     //Делегирую локальные переменные значениями объекта
     private val productList1: ObservableList<Products> by MyController.Companion::productList
 
-    override val root = tableview(productList1){
+    /*override val root = tableview(productList1){
         readonlyColumn("Продукт", Products::name)
         readonlyColumn("Стоимость", Products::coast)
         readonlyColumn("ккалории", Products::kiloCalories)
-
-
+    }*/
+    //TODO прочитать как сохранять изменения с помощью методов в column
+    override val root = tableview(productList1){
+        column("Продукты",Products::propertyName).makeEditable().onEditStartProperty()
+        column("Цена", Products::propertyCoast).makeEditable()
+        column("кКалории", Products::propertyKiloCalories).makeEditable()
     }
 }
--- src/main/kotlin/tables/Products.kt
+++ src/main/kotlin/tables/Products.kt
@@ -1,10 +1,19 @@
 package tables
 
+import tornadofx.getProperty
+import tornadofx.property
+
 /**
  * Класс содержащий список из продуктов
  */
-data class Products(val name:String, val coast:Int, val kiloCalories:Int) {
-    // val productList = mutableListOf<String>()
+data class Products(var name:String, var coast:Int, var kiloCalories:Int) {
 
+    var propertyName by property(name)
+    fun nameProperty() = getProperty(Products::propertyName)
 
+    var propertyCoast by property(coast)
+    fun CoastProperty() = getProperty(Products::propertyCoast)
+
+    var propertyKiloCalories by property(kiloCalories)
+    fun kiloCaloriesProperty() = getProperty(Products::propertyKiloCalories)
 }
