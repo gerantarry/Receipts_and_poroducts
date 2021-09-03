547baa248fbe737b426ef9f6f846610de1fcf4d5
Gerant <geraskin.antony@yandex.ru>
2021-09-04 01:20:33 +0700

    добавлены тесты по продуктам

--- src/test/kotlin/app/MyControllerTest.kt
+++ src/test/kotlin/app/MyControllerTest.kt
@@ -1,9 +1,13 @@
 package app
 
+import extendtions.upFirstChar
+import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import tornadofx.View
 import tornadofx.borderpane
+import kotlin.test.assertNotNull
 import app.MyController.Creator as MyControllerCreator
 
 class MyControllerTest{
@@ -16,18 +20,56 @@ class MyControllerTest{
         }
     }
 
+    @After
+    fun clearList(){
+        MyControllerCreator.productList.clear()
+    }
 
-    @Test
-    fun addProductToListTest(){
-        val productName = "Сыр"
-        val coast = 500
-        val kiloCalories = 877
-        controller.addProductToList(productName, coast, kiloCalories)
+
+    @Test()
+    fun simpleAddingProductTest(){
+        controller.addProductToList(productName_1, coast, kiloCalories)
         assertTrue(MyControllerCreator.productList.any {
-                    it.name==productName
+                    it.name==productName_1.upFirstChar()
                     && it.coast==coast
                     && it.kiloCalories==kiloCalories })
-
     }
+
+    @Test
+    fun findingDublicateInProductListTest(){
+        controller.addProductToList(productName_1, coast, kiloCalories)
+        controller.addProductToList(productName_2,coast,kiloCalories)
+        assertTrue("",MyControllerCreator.productList.size == 1)
+    }
+
+    @Test
+    fun removeFromProductListTest(){
+        controller.addProductToList(productName_1, coast, kiloCalories)
+        controller.removeProductFromList(productName_2)
+        assertEquals("Размер не 0! Ошибка",0,MyControllerCreator.productList.size)
+    }
+
+    @Test
+    fun removeFromProductList_notFoundTest(){
+        controller.addProductToList(productName_1, coast, kiloCalories)
+        controller.removeProductFromList("IsR")
+        assertEquals("Размер не 0! Ошибка",1,MyControllerCreator.productList.size)
+    }
+
+    @Test
+    fun loadProductFromJson(){
+        controller.loadProductListFromJson()
+        assertNotNull(MyControllerCreator.productList)
+        assertTrue(MyControllerCreator.productList.size == 3)
+    }
+
+    /*@Test
+    fun saveProductToJson(){
+        controller.addProductToList(productName_2,coast, kiloCalories)
+        controller.saveProductListAsJson()
+
+    }*/
+
+
 
 }
---
+++ src/test/kotlin/app/TestConstants.kt
@@ -0,0 +1,6 @@
+package app
+
+val productName_1 = "Сыр"
+val productName_2 = "сыР"
+val coast = 500
+val kiloCalories = 877
